02cc86f6459f1443b88e58a1221454c5083a1e09
kralljr <jenna.krall@gmail.com>
2021-09-23 10:40:14 -0400

    Fix merge issues with PM, commute, roadiness

--- rcode/roadiness_dataset.R
+++ rcode/roadiness_dataset.R
@@ -2,16 +2,15 @@
 
 library(tidyverse)
 library(lubridate)
+library(here)
+
 
 ## ============================================
-# get continuous PM data
+#  get roadiness gridcell data :
 ## ============================================
-# note: cannot merge with commute_df because R cannot distinguish between
-# duplicated rows with no differentiation
-load(here("data/pm-cont-data.RData"))
-pm <- select(datall, rdatetime, PM, ID) %>%
-  mutate(rdatetime = as_datetime(rdatetime, tz = "America/New_York")) %>%
-  unique()
+load(here("data/points_gricell.Rdata"))
+
+# get GPS data
 
 load(here("data/gpslatlon.RData"))
 cdat0 <- filter(gpslatlon$dat, missing == 1) %>%
@@ -19,31 +18,35 @@ cdat0 <- filter(gpslatlon$dat, missing == 1) %>%
   mutate(datetime= as_datetime(datetime, format = "%d %B %Y %H:%M:%S", tz = "UTC"),
          rdatetime = round_date(datetime, unit = "minute"))
 
+# bind columns:
+# THESE ARE THE SAME ORDER (ORDER PULLED FROM GPSLATLON)
+cdat0 <- bind_cols(cdat0, points_gridcell[, -c(1, 2)])
+# remove PA
+cdat0 <- filter(cdat0, !is.na(leng.distm2_scale))
 cdat <- cdat0 %>% mutate(rdatetime = as_datetime(rdatetime, tz = "America/New_York")) %>%
-  select(-missing) %>%
+  rename(rness = leng.distm2_scale) %>%
+  # remove latitude and longitude
+  dplyr::select(ID, rdatetime, rness) %>%
+  group_by(ID, rdatetime) %>%
+  summarize(rness = mean(rness)) %>%
   unique()
 
-pmdat <- inner_join(pm, cdat)
 
 ## ============================================
-#  get roadiness gridcell data :
+# get continuous PM data
 ## ============================================
-load(here("data/points_gricell.Rdata"))
-points_gridcell <- unique(points_gridcell)
+# note: cannot merge with commute_df because R cannot distinguish between
+# duplicated rows with no differentiation
+load(here("data/pm-cont-data.RData"))
+pm <- dplyr::select(datall, rdatetime, PM, ID) %>%
+  mutate(rdatetime = as_datetime(rdatetime, tz = "America/New_York")) %>%
+  unique()
 
-## ============================================
-# create roadiness dataset :
-## ============================================
 
-# inner join commutes and roadiness data :
-rpm <- left_join(pmdat, points_gridcell)
-# Remove missing: these are the commutes in eastern MD, PA
-roadiness_commutes <- na.omit(roadiness_commutes)
 
-# reorder columns
-roadiness_commutes <- roadiness_commutes[, c("Date & Time", "participant", "Longitude", "Latitude", "leng.distm2_scale", "grid_cell")]
+rcomm <- inner_join(pm, cdat)
 
 
 # save
-save(roadiness_commutes, file = "roadiness_commutes.Rdata")
+save(rcomm, file = here("data/roadiness_commutes.Rdata"))
 
