dc7393126589022e5ef5112a69c9bba436fb122e
kralljr <jenna.krall@gmail.com>
2022-01-02 10:30:52 -0500

    Fix final dataset (incl roads)

--- rcode/roadiness_dataset.R
+++ rcode/roadiness_dataset.R
@@ -9,6 +9,7 @@ library(here)
 #  get roadiness gridcell data :
 ## ============================================
 load(here("data/points_gricell.Rdata"))
+load(here("data/rtypes.RData"))
 
 # get GPS data
 
@@ -21,7 +22,9 @@ cdat0 <- filter(gpslatlon$dat, missing == 1) %>%
 
 # bind columns:
 # THESE ARE THE SAME ORDER (ORDER PULLED FROM GPSLATLON)
-cdat0 <- bind_cols(cdat0, points_gridcell[, -c(1, 2)])
+rtypes <- select(rtypes, TNMFRC)
+cdat0 <- bind_cols(cdat0, points_gridcell[, -c(1, 2)]) %>%
+  bind_cols(., rtypes)
 # remove PA
 cdat0 <- filter(cdat0, !is.na(leng.distm2_scale))
 
@@ -33,9 +36,9 @@ save(tripdata, file = here("data/tripdata.RData"))
 cdat <- cdat0 %>% mutate(rdatetime = as_datetime(rdatetime, tz = "America/New_York")) %>%
   rename(rness = leng.distm2_scale) %>%
   # remove latitude and longitude
-  dplyr::select(ID, rdatetime, rness) %>%
+  dplyr::select(ID, rdatetime, rness, TNMFRC) %>%
   group_by(ID, rdatetime) %>%
-  summarize(rness = mean(rness)) %>%
+  summarize(rness = mean(rness), rtype = mode(TNMFRC), rtype2 = mean(TNMFRC)) %>%
   unique()
 
 
@@ -44,8 +47,9 @@ cdat <- cdat0 %>% mutate(rdatetime = as_datetime(rdatetime, tz = "America/New_Yo
 ## ============================================
 # note: cannot merge with commute_df because R cannot distinguish between
 # duplicated rows with no differentiation
-load(here("data/pm-cont-data.RData"))
-pm <- dplyr::select(datall, rdatetime, PM, ID) %>%
+load(here("data/pm-cont-data-1minrti.RData"))
+pm <- dplyr::select(datall, rdatetime, rtiPM25, ID) %>%
+  rename(PM = rtiPM25) %>%
   mutate(rdatetime = as_datetime(rdatetime, tz = "America/New_York")) %>%
   unique()
 
@@ -53,6 +57,22 @@ pm <- dplyr::select(datall, rdatetime, PM, ID) %>%
 
 rcomm <- inner_join(pm, cdat)
 
+
+# add information on unique commutes
+rcomm <- mutate(rcomm, date = date(rdatetime)) %>%
+  group_by(ID, date) %>%
+  mutate(lag1 = lag(rdatetime),
+         diff = as.numeric(rdatetime - lag1),
+         diff = ifelse(is.na(lag1), 0, diff - 1),
+         cum1 = cumsum(diff)) %>%
+  ungroup() %>%
+  group_by(ID, date, cum1) %>%
+  mutate(obs = n()) %>%
+  select(-c(diff, lag1)) %>%
+  mutate(id2 = paste(ID, date, cum1), row = row_number()) %>%
+  # commutes of at least 15 minutes
+  filter(obs >= 15, !is.na(PM)) %>%
+  ungroup()
 
 # save
 save(rcomm, file = here("data/roadiness_commutes.Rdata"))
