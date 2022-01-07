499b89fdc2dcefa0c4664fa6b30be7c91a385037
kralljr <jenna.krall@gmail.com>
2022-01-07 11:00:28 -0500

    File to run all files in order to create data

---
+++ rcode/run-all.R
@@ -0,0 +1,38 @@
+# file to run all
+
+library(here)
+
+# get lat long for each trip
+# source(here("rcode/get-GMU_commute.R"))
+
+# check-commute-data.R: Check/analyze gpslatlon.RData.
+
+# get unique lat/lon
+# source(here("rcode/unique_commutes_df.R"))
+
+# get roadiness map/raster
+# source(here("rcode/roadiness_1km_plot.R"))
+
+# align roadiness with lat/lon from commutes
+# source(here("rcode/commutes_to_gridcell.R"))
+
+# get road types
+# source(here("rcode/find-roads.R"))
+
+
+########### Files usual to have to rerun
+# merge GESTDC and roadiness
+# source(here("rcode/roadiness_dataset.R"))
+
+# get VA data
+# source(here("rcode/clean-va.R"))
+
+# get weather data
+# source(here("rcode/weather.R"))
+
+# add in PM data to final dataset
+# source(here("rcode/adjust-pm.R"))
+
+# fix wind
+# source(here("rcode/wind-dir.R"))
+
